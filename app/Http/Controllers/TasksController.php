dca818c8cd614c857c65dbaf1cf4d4a2ecd4db5a
Yoichiro Ito <casharine@gmail.com>
2021-06-26 15:54:17 +0000

    CRUD tasks

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Task;
+
+class TasksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    // getでmessages/にアクセスされた場合の「一覧表示処理」
+    public function index()
+    {
+        //タスク一覧を取得
+        $tasks = Task::all();
+        
+        // タスク一覧ビューでそれを表示
+        return view('tasks.index',[
+            'tasks' => $tasks,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    // getでmessages/createにアクセスされた場合の「新規登録画面表示処理」
+    public function create()
+    {
+        $task = new Task;
+        
+        //タスク作成ビューを表示
+        return view('tasks.create', [
+            'task' => $task,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    // postでmessages/にアクセスされた場合の「新規登録処理」
+    public function store(Request $request)
+    {
+        //タスクを作成
+        $task = new task;
+        $task->content = $request->content;
+        $message->save();
+        
+        // トップページへリダイレクトさせる
+        return redirecrt('/');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    // getでmessages/（任意のid）にアクセスされた場合の「取得表示処理」
+    public function show($id)
+    {
+        // idの値でメッセージを検索して取得
+        $task = Task::findOrFail($id);
+        
+        // タスク詳細ビューでそれを表示
+        return view('tasks.show',[
+            'task' => $task
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+     // getでmessages/（任意のid）/editにアクセスされた場合の「更新画面表示処理」
+    public function edit($id)
+    {
+        //idの値でタスクを検索して取得
+        $task = Task::findOrFail($id);
+
+        // メッセージ編集ビューでそれを表示
+        return view('tasks.edit', [
+            'task' => $task,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    // putまたはpatchでmessages/（任意のid）にアクセスされた場合の「更新処理」
+
+    public function update(Request $request, $id)
+    {
+        //// idの値でタスクを検索して取得
+        $task = Task::findOrFail($id);
+        //　タスク更新
+        $task->content = $request->content;
+        $task->save();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     * 
+     */
+     // deleteでmessages/（任意のid）にアクセスされた場合の「削除処理」
+    public function destroy($id)
+    {
+        // idの値でタスクを検索して取得
+        $task = Task::findOrFail($id);
+        //　タスク削除
+        $task->delete();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,24 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// デフォルトページの表示
+Route::get('/', 'TasksController@index');
+
+Route::resource('tasks', 'TasksController');
+/*
+// メッセージの個別詳細ページ表示
+Route::get('tasks/{id}', 'TasksController@show');
+// メッセージの新規登録を処理（新規登録画面を表示するためのものではない）
+Route::post('tasks', 'TasksController@store');
+// メッセージの更新処理（編集画面を表示するためのものではない）
+Route::put('tasks/{id}', 'TasksController@update');
+// メッセージの削除 
+Route::delete('tasks', 'TasksController@destroy');
+
+// index: showの補助ページ
+Route::get('tasks', 'TasksController@index')->name('tasks.index');
+// create: storeの補助ページ、新規作成用のフォームページ
+Route::get('tasks/create', 'TasksController@create')->name('taskes.create');
+// update: putの補助ページ、更新用フォームページ
+Route::get('tasks/{id}/edit', 'TasksController@edit')->name('tasks.edit');
+*/

f55076276bd718c3c7be04c6d2342a916dca1272
Yoichiro Ito <casharine@gmail.com>
2021-07-05 05:29:58 +0000

    user login

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,18 @@
 */
 
 // デフォルトページの表示
+Route::get('/', 'TasksController@index');
+
+Route::resource('tasks', 'TasksController');
+
+// デフォルトページの表示
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+// 認証
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 /*
 // メッセージの個別詳細ページ表示
 Route::get('tasks/{id}', 'TasksController@show');

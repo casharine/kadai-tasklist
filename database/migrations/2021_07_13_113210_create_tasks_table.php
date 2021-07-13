b5f56341aee372f75aaca226994d6941ad812ccb
Yoichiro Ito <casharine@gmail.com>
2021-07-13 02:40:22 +0000

    post2

---
+++ database/migrations/2021_07_13_113210_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('content'); //カラムの追加
+            $table->string('status', 10); //カラムの追加（統合）
+            $table->timestamps();
+            
+             // 外部キー制約
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}

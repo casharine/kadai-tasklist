54eebf7608051526a40c39288595367a13538238
Yoichiro Ito <casharine@gmail.com>
2021-07-13 09:47:23 +0000

    DBrefix

--- database/migrations/2021_06_26_164333_create_tasks_table.php
+++ database/migrations/2021_06_26_164333_create_tasks_table.php
@@ -17,7 +17,6 @@ class CreateTasksTable extends Migration
             $table->bigIncrements('id');
             $table->string('content'); //カラムの追加
             $table->timestamps();
-            
         });
     }
 
